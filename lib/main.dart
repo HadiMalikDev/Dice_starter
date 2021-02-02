7423fda4f7adc1c2e06ebe15f18516a1a1e0d2fb
MHadi-Dev <Hadimalik1027@gmail.com>
2021-02-02 15:15:00 +0500

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('DiceApp',style: TextStyle(color: Colors.black),),
+        centerTitle: true,
       ),
+      body: DicePage(),
     ),
-  );
+  ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePage createState() => _DicePage();
+}
+
+class _DicePage extends State<DicePage> {
+  @override
+  
+  int dice1=1;
+  int dice2=1;
+  
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: InkWell(
+                onTap: (){
+                  setState(() {
+                    randomizer();
+                  });
+                },
+                child: Image.asset('images/dice$dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: InkWell(
+                onTap: (){
+                  setState(() {
+                    randomizer();
+                  });
+                },
+                child: Image.asset('images/dice$dice2.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  randomizer()
+  {
+    dice1=Random().nextInt(6)+1;
+    dice2=Random().nextInt(6)+1;
   }
 }
